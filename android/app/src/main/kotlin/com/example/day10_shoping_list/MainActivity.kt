ca6a51fcb46e633331decf2d2f12160a29ecb0f9
Mohammad Rahmani <rto1680@gmail.com>
2019-10-04 20:20:30 +0430

    Initail commit

---
+++ android/app/src/main/kotlin/com/example/day10_shoping_list/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.day10_shoping_list
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
